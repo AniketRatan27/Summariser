ab4439078ca03cf6e561720043642b49a96d2a47
AniketRatan27 <ratan.aniket09@gmail.com>
2025-10-07 10:34:42 +0530

    Connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import express from "express";
+import dotenv from "dotenv";
+import type {Request,Response} from 'express';
+
+dotenv.config();
+ 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/',(req:Request,res:Response)=>{
+    res.send("Hello World");
+});
+
+app.get('/api/hello',(req:Request,res:Response)=>{
+    res.send({message:"Hello World!"});
+});
+
+app.listen(port,() => {
+    console.log(`Server is running on http://localhost:${port}`);
+}); 
