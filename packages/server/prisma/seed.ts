7a033a5cad62fe4025624e8630110afa08fa14d6
AniketRatan27 <ratan.aniket09@gmail.com>
2025-12-28 14:14:48 +0530

    increased the connection time out

--- packages/server/prisma/seed.ts
+++ packages/server/prisma/seed.ts
@@ -7,6 +7,7 @@ const adapter = new PrismaMariaDb({
    user: process.env.DATABASE_USER,
    password: process.env.DATABASE_PASSWORD,
    database: process.env.DATABASE_NAME,
+   connectTimeout: 20,
 });
 
 const prisma = new PrismaClient({ adapter });
