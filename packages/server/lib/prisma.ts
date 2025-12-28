bc615fd3295fdada7105426b93f91cab7859fbf7
Aniket K Ratan <122613343+AniketRatan27@users.noreply.github.com>
2025-12-28 17:39:41 +0530

    Prisma file (#16)
    * build a prisma generic file

---
+++ packages/server/lib/prisma.ts
@@ -0,0 +1,15 @@
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "../generated/prisma";
+
+const adapter = new PrismaMariaDb({
+   host: process.env.DATABASE_HOST,
+   user: process.env.DATABASE_USER,
+   password: process.env.DATABASE_PASSWORD,
+   database: process.env.DATABASE_NAME,
+   port: Number(process.env.DATABASE_PORT),
+   connectionLimit: 20,
+});
+
+export const prisma = new PrismaClient({
+   adapter,
+});
--- packages/server/prisma/seed.ts
+++ packages/server/prisma/seed.ts
@@ -1,16 +1,4 @@
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "../generated/prisma";
-
-// SAME adapter pattern as your repositories
-const adapter = new PrismaMariaDb({
-   host: process.env.DATABASE_HOST,
-   user: process.env.DATABASE_USER,
-   password: process.env.DATABASE_PASSWORD,
-   database: process.env.DATABASE_NAME,
-   connectTimeout: 20,
-});
-
-const prisma = new PrismaClient({ adapter });
+import { prisma } from "../lib/prisma";
 
 async function main() {
    // -------------------
